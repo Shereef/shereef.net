84ecc671de27bbab6f56a66a2484e9d3cfe0e2c5
Shereef Marzouk <Git@shereef.net>
2020-03-21 06:12:50 -0300

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
