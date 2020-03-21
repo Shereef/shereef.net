99616fe4d0797fc2ee4049804bab5a2fb4e8a7c6
Shereef Marzouk <Git@shereef.net>
2020-03-21 06:13:07 -0300

    Added coming soon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     FooterComponent,
+    ComingSoonComponent
   ],
   imports: [
     BrowserModule,
