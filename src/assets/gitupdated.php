e37d14bdb6a46f74c5b8a9cb1eef634c96514cb4
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 05:39:08 -0300

    Prevent server from going down while building

--- src/assets/gitupdated.php
+++ src/assets/gitupdated.php
@@ -1,8 +1,8 @@
 <?php
 try {
     echo "Running.";
-    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && echo Done!';
-    shell_exec('nohup sh -c"' . $command . '" 2>&1 > /dev/null &');
+    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && cp -fR dist/shereef /home/shereef.net/public_html && echo Done!';
+    shell_exec('nohup sh -c"' . $command . '" > /dev/null &');
     echo "Ran.";
 } catch ( Exception $e ) {
     $msg = $e->getMessage();
--- src/assets/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /home/shereef.net/Shereef.net
-git reset --hard
-git pull --ff-only
-npm i && npm run build
-chmod 775 -R /home/shereef.net/Shereef.net
-chmod +x -R /home/shereef.net/Shereef.net
-echo Done!
