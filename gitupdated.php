87c3bc3c7994a423ca00080844871078ddc20aa0
Shereef Marzouk <356763+Shereef@users.noreply.github.com>
2021-04-30 02:39:27 -0300

    try with quotes

--- gitupdated.php
+++ gitupdated.php
@@ -1,4 +1,4 @@
 <?php
-exec('nohup cd /home/shereef.net/Shereef.net && git pull && npm i && npm run build && cp gitupdated.php /home/shereef.net/public_html/gitupdated.php > /dev/null 2>/dev/null &');
+exec('nohup "cd /home/shereef.net/Shereef.net && git pull && npm i && npm run build && cp gitupdated.php /home/shereef.net/public_html/gitupdated.php" > /dev/null 2>/dev/null &');
 echo '{"executed":true}';
 ?>
