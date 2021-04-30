e246c7fd03b7cd84571821b6e8a9161c26d5048d
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 05:32:37 -0300

    nohup

--- src/assets/gitupdated.php
+++ src/assets/gitupdated.php
@@ -1,31 +1,10 @@
 <?php
-
-/*
- * Endpoint for Github Webhook URLs
- *
- * see: https://help.github.com/articles/post-receive-hooks
- *
- */
-
-// script errors will be send to this email:
-$error_mail = "admin@shereef.net";
-
-function run() {
-    // execute update script, and record its output
-    ob_start();
-    passthru('cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && echo Done!');
-    $output = ob_end_contents();
-    //echo $output;
-    return true;
-
-}
-
 try {
     echo "Running.";
-    run();
+    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && echo Done!';
+    shell_exec('nohup sh -c"' . $command . '" 2>&1 > /dev/null &');
     echo "Ran.";
 } catch ( Exception $e ) {
     $msg = $e->getMessage();
-    mail($error_mail, $msg, ''.$e);
     echo $msg;
 }
