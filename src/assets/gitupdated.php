23617b320c7b4ca7442a1f76cea4bd705d228149
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 06:00:11 -0300

    space

--- src/assets/gitupdated.php
+++ src/assets/gitupdated.php
@@ -2,7 +2,7 @@
 try {
     echo "Running.\r\n<br />";
     $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && cp -fR dist/shereef/* /home/shereef.net/public_html --verbose && echo Done!';
-    shell_exec('nohup sh -c"' . $command . '" > $(date +"%F")$(date +"%T").log &');
+    shell_exec('nohup sh -c "' . $command . '" > $(date +"%F")$(date +"%T").log &');
     echo "Ran.\r\n<br />";
 } catch ( Exception $e ) {
     $msg = $e->getMessage();
