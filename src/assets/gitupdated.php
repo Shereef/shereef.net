f75ccb9469fe275d52a99babbdd04f3dd6034e13
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 05:53:46 -0300

    fixed file copy

--- src/assets/gitupdated.php
+++ src/assets/gitupdated.php
@@ -1,9 +1,9 @@
 <?php
 try {
-    echo "Running.";
-    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && cp -fR dist/shereef /home/shereef.net/public_html && echo Done!';
-    shell_exec('nohup sh -c"' . $command . '" > /dev/null &');
-    echo "Ran.";
+    echo "Running.\r\n<br />";
+    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && cp -fR dist/shereef/* /home/shereef.net/public_html --verbose && echo Done!';
+    shell_exec('nohup sh -c"' . $command . '" > $(date +"%F")$(date +"%T").log &');
+    echo "Ran.\r\n<br />";
 } catch ( Exception $e ) {
     $msg = $e->getMessage();
     echo $msg;
