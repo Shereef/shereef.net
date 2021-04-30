b04f8091bb694e196d30e1ff401e5ea001383e83
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 04:53:28 -0300

    chmod

--- src/assets/update.sh
+++ src/assets/update.sh
@@ -3,4 +3,6 @@ cd /home/shereef.net/Shereef.net
 git reset --hard
 git pull --rebase --ff-only
 npm i && npm run build
+chmod 775 -R /home/shereef.net/Shereef.net
+chmod +x -R /home/shereef.net/Shereef.net
 echo Done!
