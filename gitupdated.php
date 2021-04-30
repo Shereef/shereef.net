ac2fad4962128751d0e2d9fac568fea33f15c27c
Shereef Marzouk <356763+Shereef@users.noreply.github.com>
2021-04-30 02:36:39 -0300

    Trying in BG

--- gitupdated.php
+++ gitupdated.php
@@ -1,5 +1,4 @@
 <?php
-echo '<pre>';
-passthru ('cd /home/shereef.net/Shereef.net && git pull && npm i && npm run build && cp gitupdated.php /home/shereef.net/public_html/gitupdated.php');
-echo '</pre>';
+exec('nohup cd /home/shereef.net/Shereef.net && git pull && npm i && npm run build && cp gitupdated.php /home/shereef.net/public_html/gitupdated.php > /dev/null 2>/dev/null &');
+echo '{"executed":true}';
 ?>
