4ff2b0da4cc2e2c4ae7841f0fc79d665ee67863d
Shereef Marzouk <shereef.marzouk@daysmart.com>
2021-04-30 06:13:46 -0300

    clear out the folder

--- src/assets/gitupdated.php
+++ src/assets/gitupdated.php
@@ -1,7 +1,7 @@
 <?php
 try {
     echo "Running.\r\n<br />";
-    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && cp -fR dist/shereef/* /home/shereef.net/public_html --verbose && echo Done!';
+    $command = 'cd /home/shereef.net/Shereef.net && git reset --hard && git pull --ff-only && npm i && npm run build && rm -vfR /home/shereef.net/public_html/* && cp -fR dist/shereef/* /home/shereef.net/public_html --verbose && echo Done!';
     shell_exec('nohup sh -c "' . $command . '" > /home/shereef.net/logs/deployments/$(date +"%F")$(date +"%T").log &');
     echo "Ran.\r\n<br />";
 } catch ( Exception $e ) {
