28635db7437360c870e9acadb8ab92229d5f22c9
Shereef Marzouk <shereef.marzouk@daysmart.com>
2024-12-29 15:34:26 -0400

    Update Next.js configuration to set output format to 'export'

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: 'out',
+	/* config options here */
+	distDir: 'out',
+	output: 'export',
 };
 
 export default nextConfig;
