b0ec2af8950b1df5250405e49bf54966e4131c80
Shereef Marzouk <shereef.marzouk@daysmart.com>
2024-12-29 15:25:25 -0400

    Add exportPathMap to Next.js configuration for custom routing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: 'out',
+	distDir: 'out',
+	exportPathMap: async function () {
+		return {
+			'/': { page: '/' },
+		};
+	},
 };
 
 export default nextConfig;
